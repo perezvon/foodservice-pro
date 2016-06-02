dee5aaf4d8ad0cd23bbea67a23d0559210f73f02
perezvon <john.a.maloney@gmail.com>
2016-06-02 18:58:57 +0000

    inventory pdf test

--- client/js/inventory.js
+++ client/js/inventory.js
@@ -3,22 +3,35 @@ Template.inventory.helpers({
        //get all items ordered in current month
        var month = moment().format("MM");
        var year = moment().format("YYYY");
-       var monthStart = new Date(year, month, 1);
-       var monthEnd = new Date(year, (month === 12 ? 1 : month + 1), 1);
+       var monthStart = new Date(year, (month - 1), 1);
+       var monthEnd = new Date(year, (month === 11 ? 0 : month), 1);
        var ordering = Ordering.find({
            orderHistory: {$elemMatch:{date: {$gte: monthStart, $lt: monthEnd}}}}, 
            {fields: {name: 1, place: 1, orderHistory: 1}}, 
            {sort: {place: 1}
            }).fetch();
-       return ordering;
        //to do: get all items that were in stock on last month's inventory, filter duplicate productId, add to ordering, return this
        //let stock = Inventory.find({month: month, qty: {$gt: 0}}).fetch();
-   } 
+       
+       return ordering;
+   },
+   
+   month() {
+       return moment().format("MMMM YYYY");
+   }
 });
 
 Template.inventory.events({
     'click .print-pdf': function () {
-    //make a pdf and open in new window.
-        
+        var monthYear = moment().format("MMMMYYYY");
+        var doc = new PDFDocument({size: 'A4', margin: 50});
+        var stream = doc.pipe(blobStream());
+        doc.fontSize(12);
+        doc.text('PDFKit is simple', 10, 30, {align: 'center', width: 200});
+        doc.end();
+        stream.on('finish', function() {
+         window.open(stream.toBlobURL('application/pdf'), '_blank');
+        });
+        //doc.write('Inventory' + monthYear + '.pdf');
     }
-})
+});
