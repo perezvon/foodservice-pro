97f3caf561584e37c451ff325d9f8306ee6f0e5d
John Maloney <johnm@JohnMs-iMac.local>
2016-06-17 09:14:08 -0500

    refactor input invoice

--- client/js/input-invoice.js
+++ client/js/input-invoice.js
@@ -22,25 +22,26 @@ Template.newInvoice.helpers({
 
 Template.newInvoice.events({
    'submit form': function (e) {
-        e.preventDefault();
-        var currentId = $('#_id').val();
-        var data = {};
+       e.preventDefault();
+       var currentId = $('#_id').val();
+       var data = {};
+       data.date = moment($('#date').val()).toDate();
+       data.qty = $('#qty').val();
+       data.price = $('#price').val();
+       var currentPrice = 0.00;
        //get date & price history
        let currentItem = Ordering.findOne({_id: currentId}, {
             fields: {orderHistory: true}
         });
        
         let itemHistory = [];
-       if (currentItem.orderHistory){
+        if (currentItem.orderHistory){
             let curr = currentItem.orderHistory; 
             for (let i = 0; i < curr.length; i++){
-            let price = parseInt((curr[i].price)).toFixed(2);
-            itemHistory.push({date: moment(curr[i].date).toDate(), price: price});
+                let price = parseInt((curr[i].price)).toFixed(2);
+                itemHistory.push({date: moment(curr[i].date).toDate(), price: price});
             }
             itemHistory = _.sortBy(itemHistory, 'date').reverse();
-            data.date = moment($('#date').val()).toDate();
-            data.qty = $('#qty').val();
-            data.price = $('#price').val();
             //check if entered date is latest date, then check whether this price is different than item.price & update if needed
             if (data.date > itemHistory[0].date) {
                 if (data.price !== itemHistory[0].price) {
@@ -48,7 +49,10 @@ Template.newInvoice.events({
                 }    
             }
         }
-        
+        else {
+            currentPrice = data.price;
+            Meteor.call('updateOrderGuide', {_id: currentId}, {$set: {price: currentPrice}});
+        }
        //add the order record to the item's history
         Meteor.call('updateOrderGuide', {_id: currentId}, {$addToSet:{orderHistory: data}}, function(error){
               if (error) Bert.alert(error.reason, 'danger');
