49c5a900e314a3ca2f685c70292183003ce2a3a7
John A Maloney <johnamaloney@Johns-MacBook-Pro-2.local>
2017-06-26 13:21:31 -0500

    last month stock to fixed decimal places

--- client/inventory/inventory.js
+++ client/inventory/inventory.js
@@ -53,6 +53,7 @@ Template.inventory.helpers({
            });
            ordering = _.uniq(ordering, true, function(a){return a.productId;});
            ordering.forEach(function(item) {
+             item.qty = parseFloat(item.qty).toFixed(2)
              item.orderedThisMonth = item.orderHistory ? item.orderHistory.filter(function(x){return x.date >= monthStart && x.date < monthEnd}).reduce(function(a,b){return a + parseInt(b.qty)}, 0) : ""
            })
        }
